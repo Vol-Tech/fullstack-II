56276e9cd792ac07075264fced8d59d8efe64823
CyCklop <ignacioandresflores06@gmail.com>
2026-08-27 23:06:12 -0400

    feat: se comento el script que use en js para mostrar un producto, testie a ver si se podia con un diccionario de objetos en js, y se puede, de momento solo muestro 1 key del diccionario, porque estoy craneando a ver si puedo mostrar todas en cada producto correspondiente del html, pero sin escribir codigo repetitivo y de mala practica

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // Test de guardar productos
 
+/*
 const producto = {
     nombreProducto: 'Cable HDMI',
     precio: 4500
 }
+*/
 
-// se pasa de objeto a texto con stringify para que localStorage lo lea
+/* se pasa de objeto a texto con stringify para que localStorage lo lea
 localStorage.setItem('producto', JSON.stringify(producto));
 
 // se usa para recuperar el 'objeto'
@@ -18,3 +20,20 @@ const precioSpan = document.getElementById('precio-producto');
 // textContent lee o modifica el texto de forma pura (osea segu su etiqueta)
 nombreSpan.textContent = data.nombreProducto;
 precioSpan.textContent = data.precio;
+
+//test de tener muchos productos?*/
+
+const inventario = {
+    'PROD01': { nombre: "Cable HDMI", precio: 4500 },
+    'PROD02': { nombre: "Cable DVI", precio: 7500 },
+    'PROD03': { nombre: "Monitor ASUS 120Hz", precio: 150000 }
+};
+
+localStorage.setItem('inventario', JSON.stringify(inventario));
+const data = JSON.parse(localStorage.getItem('inventario'));
+
+const nombreSpan1 = document.getElementById('nombre-producto');
+const precioSpan1 = document.getElementById('precio-producto');
+
+nombreSpan1.textContent = data['PROD01'].nombre;
+precioSpan1.textContent = data['PROD01'].precio;
